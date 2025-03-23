0e3453a323036ed402be5d32c8cff08b239d7a4e
Magdalena-N <magdusiamn@gmail.com>
2025-03-23 11:34:51 +0100

    add test case with buying

--- pages/main.page.ts
+++ pages/main.page.ts
@@ -4,12 +4,27 @@ import { Page, expect, Locator } from "@playwright/test";
 export class MainPage {
   loginInput: Locator;
   passwordInput: Locator;
+  loginOrRegisterButton: Locator;
   titlePage: string;
+  makeupCategoryTab: Locator;
+  cheeksCategory: Locator;
+  mainTextLocator: Locator;
 
   constructor(protected page: Page) {
     this.loginInput = this.page.locator("#loginFrm_loginname");
     this.passwordInput = this.page.locator("#pass");
     this.titlePage = "A place to practice your automation skills!";
+    this.loginOrRegisterButton = this.page.getByRole("link", {
+      name: "Login or register",
+    });
+    this.makeupCategoryTab = this.page
+      .locator("#categorymenu")
+      .getByRole("link", { name: "Makeup" });
+    this.cheeksCategory = this.page
+      .locator("#categorymenu")
+      .getByRole("link", { name: "Cheeks" });
+
+    this.mainTextLocator = this.page.locator(".maintext");
   }
 
   async fillInputPassword(password: string): Promise<void> {
@@ -22,8 +37,14 @@ export class MainPage {
   }
 
   async clickLoginOrRegister(): Promise<void> {
-    await this.page.getByRole('link', { name: 'Login or register' }).click();
-    await expect(this.page).toHaveTitle("Account Login")
+    await this.loginOrRegisterButton.click();
+    await expect(this.page).toHaveTitle("Account Login");
+  }
+
+  async gotoMakeupCheeksCategory(): Promise<void> {
+    await this.makeupCategoryTab.hover();
+    await this.cheeksCategory.click();
+    await expect(this.mainTextLocator).toContainText("Cheeks");
   }
 
   async verifyCorrectLogin(): Promise<void> {
--- tests/pw.spec.ts
+++ tests/pw.spec.ts
@@ -2,12 +2,32 @@ import { test } from "../fixtures/base";
 import "dotenv/config";
 import { data, storeUrl } from "../helpers/data";
 
-test("test", async ({ mainPage, loginPage, registerPage, page }) => {
+test.skip("test", async ({ mainPage, loginPage, registerPage, page }) => {
   await mainPage.openMainPage(storeUrl);
   await mainPage.clickLoginOrRegister();
   await loginPage.clickRegisterNewCustomer();
   await registerPage.fillNewAccount(data);
   await registerPage.selectNewsletterOption("No");
   await registerPage.acceptPrivacyPolicy(true);
-  // await registerPage.clickContinue()
+  await registerPage.clickContinue();
 });
+
+test("login_buy", async ({ mainPage, loginPage, registerPage, page }) => {
+  await test.step("Login", async () => {
+    await mainPage.openMainPage(storeUrl);
+    await mainPage.clickLoginOrRegister();
+    await loginPage.fillLoginInputs(
+      process.env.USER as string,
+      process.env.PASSWORD as string
+    );
+    await loginPage.clickLoginButton();
+    await loginPage.verifyCorrectLogin();
+  });
+
+  await test.step("Add to cart", async () => {
+    await mainPage.openMainPage(storeUrl);
+    await mainPage.gotoMakeupCheeksCategory();
+  });
+});
+
+
